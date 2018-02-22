8a24a9acba69c52c17f1e1db41e225463063277e
Unknown <Sean.Purvis@usm.edu>
2018-02-22 11:22:47 -0600

    Add Users Reducer

--- src/users/reducer.js
+++ src/users/reducer.js
@@ -1,11 +1,14 @@
 import {
   USER_CREATING,
   USER_CREATE_SUCCESS,
-  USER_CREATE_ERROR
+  USER_CREATE_ERROR,
+  USER_REQUESTING,
+  USER_REQUEST_SUCCESS,
+  USER_REQUEST_ERROR
 } from './constants'
 
 const initialState = {
-  list: [], // where users are stored
+  list: [],
   requesting: false,
   successful: false,
   messages: [],
@@ -28,6 +31,7 @@ const reducer = function userReducer(state = initialState, action) {
         errors: []
       }
 
+    // On Success include the new user into the list
     case USER_CREATE_SUCCESS:
       return {
         list: state.list.concat([action.user]),
@@ -35,7 +39,7 @@ const reducer = function userReducer(state = initialState, action) {
         successful: true,
         messages: [
           {
-            body: `User: ${action.user.username} created.`,
+            body: `User: ${action.user.username} created!`,
             time: new Date()
           }
         ],
