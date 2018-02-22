63815bf53f016476d7500b673b18c0abe81a2b56
Unknown <Sean.Purvis@usm.edu>
2018-02-22 12:04:51 -0600

    Add POST Method to User Form
    Add Create User functionality to the users page

--- src/users/index.js
+++ src/users/index.js
@@ -37,86 +37,126 @@ class Users extends Component {
   }
 
   render() {
+    const {
+      handleSubmit,
+      invalid,
+      users: { list, requesting, successful, messages, errors }
+    } = this.props
     return (
       <div className="users">
-        <form className="user-form">
-          <h2>CREATE USER</h2>
-          <label htmlFor="username">Username:</label>
-          <Field
-            name="username"
-            type="text"
-            id="username"
-            className="username"
-            label="Username"
-            component="input"
-          />
-          <br />
-          <label htmlFor="name">Name:</label>
-          <Field
-            name="name"
-            type="text"
-            id="name"
-            className="name"
-            label="name"
-            component="input"
-          />
-          <br />
-          <label htmlFor="phone">Phone:</label>
-          <Field
-            name="phone"
-            type="text"
-            id="phone"
-            className="phone"
-            label="phone"
-            component="input"
-          />
-          <br />
-          <label htmlFor="password">Password:</label>
-          <Field
-            name="password"
-            type="password"
-            id="password"
-            className="password"
-            label="password"
-            component="input"
-          />
-          <br />
-          <label htmlFor="employer">Employer:</label>
-          <Field
-            name="employer"
-            type="text"
-            id="employer"
-            className="employer"
-            label="employer"
-            component="input"
-          />
-          <br />
-          <label htmlFor="email">Email:</label>
-          <Field
-            name="email"
-            type="email"
-            id="email"
-            className="email"
-            label="email"
-            component="input"
-          />
-          <br />
-          <label htmlFor="role">Role:</label>
-          <Field
-            name="role"
-            id="role"
-            className="role"
-            label="role"
-            component="select">
-            <option />
-            <option value="adminjail">adminjail</option>
-            <option value="admincourt">admincourt</option>
-            <option value="nurse">nurse</option>
-            <option value="jailer">jailer</option>
-            <option value="clerk">clerk</option>
-            <option value="counselor">counselor</option>
-          </Field>
-        </form>
+        <div className="user-form">
+          <form onSubmit={handleSubmit(this.submit)}>
+            <h2>CREATE USER</h2>
+            <label htmlFor="username">Username:</label>
+            <Field
+              name="username"
+              type="text"
+              id="username"
+              className="username"
+              label="Username"
+              component="input"
+            />
+            <br />
+            <label htmlFor="name">Name:</label>
+            <Field
+              name="name"
+              type="text"
+              id="name"
+              className="name"
+              label="name"
+              component="input"
+            />
+            <br />
+            <label htmlFor="phone">Phone:</label>
+            <Field
+              name="phone"
+              type="text"
+              id="phone"
+              className="phone"
+              label="phone"
+              component="input"
+            />
+            <br />
+            <label htmlFor="password">Password:</label>
+            <Field
+              name="password"
+              type="password"
+              id="password"
+              className="password"
+              label="password"
+              component="input"
+            />
+            <br />
+            <label htmlFor="employer">Employer:</label>
+            <Field
+              name="employer"
+              type="text"
+              id="employer"
+              className="employer"
+              label="employer"
+              component="input"
+            />
+            <br />
+            <label htmlFor="email">Email:</label>
+            <Field
+              name="email"
+              type="email"
+              id="email"
+              className="email"
+              label="email"
+              component="input"
+            />
+            <br />
+            <label htmlFor="role">Role:</label>
+            <Field
+              name="role"
+              id="role"
+              className="role"
+              label="role"
+              component="select">
+              <option />
+              <option value="adminjail">adminjail</option>
+              <option value="admincourt">admincourt</option>
+              <option value="nurse">nurse</option>
+              <option value="jailer">jailer</option>
+              <option value="clerk">clerk</option>
+              <option value="counselor">counselor</option>
+            </Field>
+            <br />
+            <button action="submit">CREATE</button>
+          </form>
+        </div>
+        {/* <div className="user-list">
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Phone</th>
+                <th>Employer</th>
+                <th>Role</th>
+                <th>Email</th>
+              </tr>
+            </thead>
+            <tbody>
+              {list &&
+                !!list.length &&
+                list.map(user => (
+                  <tr key={user.id}>
+                    <td>
+                      <strong>{`${user.username}`}</strong>
+                    </td>
+                    <td>{`${user.name}`}</td>
+                    <td>{`${user.phone}`}</td>
+                    <td>{`${user.employer}`}</td>
+                    <td>{`${user.role}`}</td>
+                    <td>{`${user.email}`}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+          <button onClick={this.fetchUsers}>Refetch Users!</button>
+        </div> */}
       </div>
     )
   }
